29c91fdca087233ac5813cc8f46777c6589fbd7e
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-04-18 01:18:27 +0300

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace jwtoken.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string userName, string password)
+        {
+            var claims = new[]{
+                new Claim(ClaimTypes.Name,userName),
+                new Claim(JwtRegisteredClaimNames.Email,userName)
+            };
+
+            var singinKey = "BuBenimSigningKey";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(singinKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: "https://ww.emreavarisli.com",
+                audience: "BuBenimKullanıdığımAudienceDegeri",
+                claims: claims,
+                expires: DateTime.Now.AddDays(15),
+                notBefore: DateTime.Now,
+                signingCredentials: credentials
+            );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            return token;
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace jwtoken
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
